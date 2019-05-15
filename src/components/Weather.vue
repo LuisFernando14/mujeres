26abd8bdb04ff37d2dbe026f91bab100c9a1cd79
Mario Fonseca <mfmtz15.agri@gmail.com>
2019-05-15 12:32:03 -0600

    Tabla Climas Bootstrap

--- src/components/Weather.vue
+++ src/components/Weather.vue
@@ -1,66 +1,68 @@
 <template>
-<v-layout align-center justify-center row fill-height>
-    <v-flex xs12 sm10>
-      <v-card>
-        <v-container
-          fluid
-          grid-list-md
-        >
-        <v-toolbar color="green" dark>
-        <v-toolbar-title class="display-1">Aguacate</v-toolbar-title>
-        <v-spacer></v-spacer>
-        
-      </v-toolbar>
+  <div class="container">
+    <table class="table">
+      <thead class="thead-dark">
+        <tr class>
+          <th scope="col" colspan="4" class="bg-success">
+            <h1 class="display-1">Aguacate</h1>
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td v-for="card in cards" :key="card.title">
+            <div class="card text-center">
+              <div class="card-header">
+                <h4>{{card.state}}</h4>
+              </div>
 
-          <v-layout row wrap>
-            <v-flex
-              v-for="card in cards"
-              :key="card.title"
-              v-bind="{ [`xs${card.flex}`]: true }"
-            >
-              <v-card >
-             
-                <!---->
-                <v-card-title class="headline font-weight-bold">
-                    {{card.title}}
-                </v-card-title>
-                <v-card-text>
-               <!--<v-img :src="card.src"></v-img>-->
-               <v-container>
-                   <v-layout>
-                       <v-flex><img v-bind:src="card.src" style="width:70%"></v-flex>
-                       <v-flex><h2 class="display-3"> {{card.grados}}</h2></v-flex>
-                   </v-layout>
-               </v-container>
+              <div class="card-body">
+                <img v-bind:src="card.src" style="width:80px">
 
- 
-                </v-card-text>
-                
-                <!---->
-                
-                
-
-              </v-card>
-            </v-flex>
-          </v-layout>
-        </v-container>
-      </v-card>
-    </v-flex>
-  </v-layout>
-
-
-
-
+                <h5 class="card-title">{{card.title}}</h5>
+                <h1 class="display-1">{{card.grade}}</h1>
+              </div>
+            </div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+    <!---->
+  </div>
 </template>
 <script>
-  export default {
-    data: () => ({
-      cards: [
-        { title: 'Aguascalientes, Aguascalientes', src: '//cdn.apixu.com/weather/64x64/day/113.png', flex: 3,grados:'32°' },
-        { title: 'Los Mochis, Sinaloa', src: '//cdn.apixu.com/weather/64x64/day/116.png', flex: 3 ,grados:'32°'},
-        { title: 'La Huacana, Michoacán', src: '//cdn.apixu.com/weather/64x64/day/299.png', flex: 3 ,grados:'32°'},
-        { title: 'AltaMirano, Chiapas', src: '//cdn.apixu.com/weather/64x64/day/122.png', flex: 3 ,grados:'32°'}
-      ]
-    })
-  }
+export default {
+  data: () => ({
+    cards: [
+      {
+        title: "Los Mochis",
+        state: "Sinaloa",
+        src: "//cdn.apixu.com/weather/64x64/day/116.png",
+        flex: 3,
+        grade: "27°"
+      },
+      {
+        title: "La Huacana",
+        state: "Michoacán",
+        src: "//cdn.apixu.com/weather/64x64/day/299.png",
+        flex: 3,
+        grade: "20°"
+      },
+      {
+        title: "AltaMirano",
+        state: "Chiapas",
+        src: "//cdn.apixu.com/weather/64x64/day/122.png",
+        flex: 3,
+        grade: "16°"
+      },
+      {
+        title: "Obregón",
+        state: "Sonora",
+        src: "//cdn.apixu.com/weather/64x64/day/113.png",
+        flex: 3,
+        grade: "32°"
+      }
+    ]
+  })
+};
 </script>
