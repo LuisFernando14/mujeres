f3ac54df0aa235148f07c91b374c2ca15c8a8823
RubenGarica1 <rubengarcia182@gmail.com>
2019-05-16 18:02:32 -0600

    [MOD] cambios diferentes

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,11 +1,7 @@
 <template>
   <div>
-<<<<<<< HEAD
     <MegaNoticia />
     <Weather />
-=======
-    <News />
->>>>>>> 27ef9f952864c63ad25fad0e6ba39a4af5807ab6
   </div>
 </template>
 
@@ -13,22 +9,14 @@
 import Weather from '@/components/Weather';
 import NewsComp from '@/components/NewsComp';
 import MainNew from '@/components/MainNew';
-<<<<<<< HEAD
 import MegaNoticia from '@/components/MegaNoticia';
-=======
-import News from '@/components/News';
->>>>>>> 27ef9f952864c63ad25fad0e6ba39a4af5807ab6
 export default {
   name:'Home',
   components: {
     Weather,
     NewsComp,
     MainNew,
-<<<<<<< HEAD
     MegaNoticia
-=======
-    News,
->>>>>>> 27ef9f952864c63ad25fad0e6ba39a4af5807ab6
   }
 }
 </script>
