1271d2f077bbfb34dac459588dd962dd678ee818
Mario Fonseca <mfmtz15.agri@gmail.com>
2019-05-17 17:04:28 -0600

    Imagenes en noticias

--- src/components/News.vue
+++ src/components/News.vue
@@ -53,68 +53,80 @@ export default {
   data: () => ({
       new1:[
           {
-            title:"Mouse negro",
+            title:"EE. UU. comienza a importar el chile más picante",
             date:"17 de Mayo",
-            body:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor ",
+            body:"Han comenzado las importaciones de chile Carolina Reaper en Estados Unidos.",
+            img:"https://portal-data1.storage.googleapis.com/news/1558026964728_background-cayenne-chili-1091778.jpg"
           },
           {
-            title:"Mouse blanco",
+            title:"México prepara nuevos gravámenes para los productos estadounidenses",
             date:"17 de Mayo",
-            body:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor",
+            body:"El tomate, uno de los alimentos más consumidos del mundo.",
+            img:"https://portal-data1.storage.googleapis.com/news/1558027099270_fruits-market-colors.jpg"
           },
           {
-            title:"Mouse gris",
+            title:"Proyecto de mapeo de genoma del tomate sería clave para desbloquear mejor sabor",
             date:"17 de Mayo",
-            body:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor",
+            body:"Científicos del Servicio de Investigación Agrícola de EE.UU.",
+            img:"https://portal-data1.storage.googleapis.com/news/1558027253084_pexels-photo-428301-(1).jpeg"
           },
           {
-            title:"Mouse obscuro",
+            title:"México podría exportar plátano a China durante 2019",
             date:"17 de Mayo",
-            body:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor",
+            body:"Con la firma del acuerdo fitosanitario entre China y México, se espera que este último pueda empezar a comercializar el plátano antes de que finalice el año.",
+            img:"https://portal-data1.storage.googleapis.com/news/1558027392119_banana-fruit-healthy-yellow-41957.jpeg"
           }
       ],
       new2:[
           {
-            title:"Mouse rojo",
+            title:"Presenta Seipasa su nuevo departamento de microbiología y fortalece su apuesta por la I+D+i",
             date:"17 de Mayo",
-            body:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor",
+            body:"Seipasa, empresa española especializada en el desarrollo y formulación de biopesticidas, bioestimulantes y fertilizantes para la agricultura.",
+            img:"https://portal-data1.storage.googleapis.com/news/1558027677856_biology-doctor-health-4154.jpg"
           },
           {
-            title:"Mouse amarillo",
+            title:"Lanzamiento de tres nuevas variedades de frambuesa «Made in Chile»",
             date:"17 de Mayo",
-            body:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor",
+            body:"Santa Teresa, Santa Clara y Santa Catalina son las tres variedades de frambuesas 100% chilenas desarrolladas por el Programa de Mejoramiento Genético",
+            img:"https://portal-data1.storage.googleapis.com/news/1558027999014_background-berries-close-up-1537690.jpg"
           },
           {
-            title:"Mouse Naranja",
+            title:"La innovación es el camino para el sector de berries de México y Florida",
             date:"17 de Mayo",
-            body:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor",
+            body:"Recientemente, la Asociación de Productores de Fresas de Florida (FSGA, por sus siglas en inglés) expresó su preocupación por los precios decrecientes de las fresas que están enfrentando los productores de Florida.",
+            img:"https://portal-data1.storage.googleapis.com/news/1557936202750_accion-color-comida-1070980.jpg"
           },
           {
-            title:"Mouse amarojo",
+            title:"Un protector que reduce en un 90% la necesidad de agua y fertilizante de los árboles",
             date:"17 de Mayo",
-            body:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor",
+            body:"Beneficioso para los agricultores y para el medio ambiente. Así se presenta Tree T-PEE, un contenedor de agua y nutrientes creado con plástico reciclado que se ubica en la base de los árboles para protegerlos del frío y proveerlos del agua y el fertilizante que necesitan, ni una gota más.",
+            img:"https://portal-data1.storage.googleapis.com/news/1557944184119_forest-path-trees-6037.jpg"
           }
       ],
       new3:[
           {
-            title:"Mouse morado",
+            title:"Los mecanismos del envejecimiento vegetal",
             date:"16 de Mayo",
-            body:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor",
+            body:"A diferencia de los animales que pueden evitar el estrés, las plantas han desarrollado respuestas para evadir factores que pueden ser perjudiciales para el crecimiento y la supervivencia.",
+            img:"https://portal-data1.storage.googleapis.com/news/1557944417185_agriculture-cornfield-crops-55766.jpg"
           },
           {
-            title:"Mouse lila",
+            title:"Cómo la agricultura nos convirtió en X-Men",
             date:"16 de Mayo",
-            body:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor",
+            body:"A pesar de la aureola idílica que rodea a la agricultura y de que nos conecta a la naturaleza, al más puro estilo flower power, lo cierto es que no solo fue producto de los primeros ingenieros genéticos de la historia",
+            img:"https://portal-data1.storage.googleapis.com/news/1557945279941_canola-fields-1911392_1280.jpg"
           },
           {
-            title:"Mouse rosita",
+            title:"Propone Sinaloa imponer arancel a maíz de EU",
             date:"16 de Mayo",
-            body:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor ",
+            body:"Ante las imposiciones de aranceles al tomate mexicano, el gobierno federal debería gravar con un arancel similar las importaciones de maíz, señaló el secretario de Agricultura y Ganadería de Sinaloa, Manuel Tarriba Urtuzuastegui.",
+            img:"https://portal-data1.storage.googleapis.com/news/1557945472078_agriculture-corn-cropland-96715.jpg"
           },
           {
-            title:"Mouse purpura",
+            title:"La innovación es el camino para el sector de berries de México y Florida",
             date:"16 de Mayo",
-            body:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor",
+            body:"Recientemente, la Asociación de Productores de Fresas de Florida (FSGA, por sus siglas en inglés) expresó su preocupación por los precios decrecientes de las fresas que están enfrentando los productores de Florida.",
+            img:"https://portal-data1.storage.googleapis.com/news/1557936202750_accion-color-comida-1070980.jpg"
           }
       ]
       
--- src/components/deckNews.vue
+++ src/components/deckNews.vue
@@ -4,7 +4,7 @@
       <!---->
       <div class="card-deck card bg-light" style="padding-top:2%;padding-bottom:2%">
         <div v-for="card in new1" :key="card.city" class="card border-light ">
-          <img src="https://picsum.photos/800/700" class="card-img-top" alt="...">
+          <img v-bind:src="card.img" class="card-img-top" alt="..." style="width:200px;height:150px">
           <div class="card-body">
             <h5 class="card-title">{{card.title}}</h5>
             <p class="card-text">{{card.body}}</p>
