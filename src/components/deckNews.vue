babbefc07d1b50d004f0f5dc2d66ee927cfb627c
Mario Fonseca <mfmtz15.agri@gmail.com>
2019-05-21 11:29:00 -0600

    Noticias Responsivo

--- src/components/deckNews.vue
+++ src/components/deckNews.vue
@@ -3,19 +3,51 @@
     <div class="container-fluid">
       <!---->
       <div class="card-deck card bg-light" style="padding-top:2%;padding-bottom:2%">
-        
-        
-        <div v-for="card in new1" :key="card.city" class="card border-light ">
-          <img v-bind:src="card.img" class="card-img-top" alt="..." style="width:200px;height:150px">
+        <div v-for="card in new1" :key="card.city" class="card border-light">
+          <img
+            v-bind:src="card.img"
+            class="card-img-top news-img"
+            alt="..."
+          >
           <div class="card-body">
             <h5 class="card-title">{{card.title}}</h5>
-            <p class="card-text">{{card.body}}</p>
+            <p class="card-text">{{card.short}}</p>
             <p class="card-text">
               <small class="text-muted">{{card.date}}</small>
             </p>
-            
           </div>
+          <div class="card-footer">
+            <button
+              type="button"
+              class="btn btn-primary"
+              data-toggle="modal"
+              data-target="#exampleModalCenter"
+            >Ver más</button>
+          </div>
+          <!---->
           
+<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalCenterTitle">Hola</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio nostrum porro ab sit, neque ullam culpa harum amet nulla pariatur suscipit temporibus illum optio magni quidem molestias aliquid nemo et.
+      </div>
+      <div class="modal-footer">
+        
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+          <!---->
         </div>
       </div>
 
@@ -30,4 +62,18 @@ export default {
 };
 </script>
 <style >
+.news-img{
+  width:200px;
+  height:150px;
+}
+@media only screen and (max-width: 600px) {
+   .news-img{
+    width:50px;
+    height:200px;
+    
+  }
+}
+
+
+
 </style>
