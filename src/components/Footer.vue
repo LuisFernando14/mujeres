749d01cab04ba2d1e18f1604e182579410cd9eaf
Mario Fonseca <mfmtz15.agri@gmail.com>
2019-05-21 12:54:01 -0600

    Footer Responsivo

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,44 +2,39 @@
   <footer style="background-color:#2e2e2f;color:white;">
     <br>
     <div class="container">
-      <br>
-      <br>
-      <br>
-      <div class="row justify-content-md-center">
-        <div class="col col-lg-2">
+      <div class="row justify-content-center">
+        <div class="col-7 col-md-10  d-flex justify-content-center">
           <a href>
             <img src="https://agrimercante.com/images/footer/f-logo-neg-png.png" alt>
           </a>
         </div>
       </div>
-      <br>
-      <div class="row justify-content-md-center">
-        <div class="col col-lg-6">
-          <hr style="border-top: 2px solid white;width: 100%;">
+      <div class="row justify-content-center">
+        <div class="col-7 col-md-10  d-flex justify-content-center">
+          <hr style="border-top: 2px solid white;width: 70%;">
           <br>
         </div>
       </div>
-      <div class="row justify-content-md-center">
-        <div class="col col-lg-2 text-center">
+      <div class="row justify-content-center">
+        <div class="col col-md-2 d-flex justify-content-center">
           <a class="aaa" href>
             <span class="f-letra">Términos de uso</span>
           </a>
         </div>
-        <div class="col col-lg-2 text-center">
+        <div class="col col-md-2  d-flex justify-content-center">
           <a class="aaa" href>
             <span class="f-letra">Privacidad</span>
           </a>
         </div>
-        <div class="col col-lg-2 text-center">
+        <div class="col col-md-2  d-flex justify-content-center">
           <a class="aaa" href>
             <span class="f-letra">Contáctanos</span>
           </a>
         </div>
       </div>
+      
     </div>
-    <br>
-    <br>
-    
+    <br><br>
   </footer>
 </template>
 <script>
@@ -60,5 +55,13 @@ export default {
 }
 .aaa:hover {
   text-decoration: none; /* no underline */
+}
+
+
+
+@media only screen and (max-width: 600px) {
+   .f-letra{
+    font-size: 0.8em;
+  }
 }
 </style>
