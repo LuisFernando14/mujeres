11197437451a90aed89f01cf58aa2bf94a7569bf
Ruben A Garcia Acosta <ruben@MacBook-Pro-de-Ruben.local>
2019-05-28 15:42:18 -0600

    [FIX] se eliminaron algunas librerias sin unsar

--- src/main.js
+++ src/main.js
@@ -5,18 +5,17 @@ import axios from 'axios';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/css/styles.css'
-import vueCountryRegionSelect from 'vue-country-region-select'
-import Places from "google-places-web";
 import VueHead from 'vue-head';
+
+// import the styles 
 
 Vue.use(VueHead)
 
-axios.defaults.baseURL = 'https://users220190523045749.azurewebsites.net'; // same as the Url the server listens to
-Vue.prototype.$http = axios;
+//axios.defaults.baseURL = 'https://users220190523045749.azurewebsites.net'; // same as the Url the server listens to
+//Vue.prototype.$http = axios;
 // https://users220190523045749.azurewebsites.net/api/users
 
 Vue.config.productionTip = false;
-Vue.use(vueCountryRegionSelect);
 
 
 new Vue({
