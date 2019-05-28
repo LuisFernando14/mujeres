f9df23b6b78048e222556b392e96cdd06e748081
Ruben A Garcia Acosta <ruben@MacBook-Pro-de-Ruben.local>
2019-05-28 16:56:42 -0600

    [MOD] cambios

--- src/main.js
+++ src/main.js
@@ -5,21 +5,20 @@ import axios from 'axios';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/css/styles.css'
-import VueHead from 'vue-head';
-
-// import the styles 
-
+import VueHead from 'vue-head'
+ 
 Vue.use(VueHead)
 
-//axios.defaults.baseURL = 'https://users220190523045749.azurewebsites.net'; // same as the Url the server listens to
-//Vue.prototype.$http = axios;
+
+axios.defaults.baseURL = 'https://users220190523045749.azurewebsites.net'; // same as the Url the server listens to
+Vue.prototype.$http = axios;
 // https://users220190523045749.azurewebsites.net/api/users
 
 Vue.config.productionTip = false;
 
 
 new Vue({
-  router,
-  axios,
-  render: h => h(App),
+ router,
+ axios,
+ render: h => h(App),
 }).$mount('#app');
